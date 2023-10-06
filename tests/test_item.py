1f7e4c11fa9c9306f249b37d8907e4278c1908d9
Yuriy Sorochinskiy <yuriy.ys314@mail.ru>
2023-10-06 14:44:31 +0600

    tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -19,7 +19,21 @@ def test_apply_discount():
     product_2.apply_discount()
     assert product_2.price == 3000
 
-
 def test_string_to_number():
     p1 = Item('Product', 300, 3)
     assert p1.string_to_number('7') == 7
+
+def test_item():
+    product_3 = Item('Product', 300, 2)
+    assert product_3.name == 'Product'
+    assert product_3.price == 300
+    assert product_3.quantity == 2
+
+def test_setter(item):
+    item.name = 'Шляпа678910'
+    assert item.name == 'Шляпа67891'
+
+path = '../src/items.csv'
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('../src/items.csv')
+    assert len(Item.all) == 5
